9895a8ba2fa15a31f706ca57cc42623fedc6936b
techmore <sdolbec1@gmail.com>
2018-03-21 14:07:15 -0400

    Update refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -2,5 +2,9 @@
 
 rm -R techmore.github.io-master
 rm master.zip
+rm *.html *.jpg *.png *.bak 
 wget https://github.com/techmore/techmore.github.io/archive/master.zip
-unzip master.zip
+unzip master.zip 
+cd techmore.github.io-master
+mv * ../*
+
